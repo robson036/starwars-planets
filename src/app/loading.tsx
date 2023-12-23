e1ec447f8ec42d864980af543491c64cefad0ff0
Robson <robson.miranda1991@gmail.com>
2023-12-22 23:15:08 -0300

    ✨feat: added loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,14 @@
+import { ArrowPathIcon } from "@heroicons/react/24/outline"
+
+export default function Loading() {
+    return (
+        <div className="w-[37rem] mt-14 rounded-xl bg-white mx-auto max-md:w-11/12 py-12 flex justify-center items-center">
+            <ArrowPathIcon
+                width={32}
+                height={32}
+                color="#DE1212"
+                className="animate-spin"
+            />
+        </div>
+    )
+}
